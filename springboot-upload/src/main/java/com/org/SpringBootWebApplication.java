d9ef1c55a8638eee8f96d20685b2f70997f07eda
Raghun845 <raghun845@gmail.com>
2017-08-31 00:24:46 -0400

    new proj

---
+++ springboot-upload/src/main/java/com/org/SpringBootWebApplication.java
@@ -0,0 +1,33 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.agilegroup.co.jp/technote/springboot-fileupload-error-handling.html
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    //Tomcat large file upload connection reset
+    /*@Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+
+    }*/
+
+}
